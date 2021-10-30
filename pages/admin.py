11bcede3be3cf2b636afde47b8a1f6b81bb50d33
tanmay-dash91 <tanmaybls1991@gmail.com>
2021-10-30 22:01:32 +0530

    completed team model

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Team
+from django.utils.html import  format_html
 
 # Register your models here.
+
+class Teamadmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius:50px"/>'.format(object.photos.url))
+
+    thumbnail.short_description = 'Image'
+    list_display =('id','thumbnail','firstname','designation','created_date')
+    list_display_links = ('id','thumbnail','firstname',)
+    serach_fields = ('firstname','lastname','designation')
+    list_filter = ('designation',)
+
+admin.site.register(Team,Teamadmin)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Team(models.Model):
+
+
+    firstname = models.CharField(max_length=250)
+    lastname = models.CharField(max_length=250)
+    designation = models.CharField(max_length=250)
+    photos = models.ImageField(upload_to = 'photos/%Y/%m/%d/')
+    fblink = models.URLField(max_length=100)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.firstname
